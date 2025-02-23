2cf3123cde2934b71df95ff8f00d974550363573
filipesa <filipe.santos@duxus.com.br>
2025-02-23 15:44:31 -0300

    Setup inicial

---
+++ src/main/java/br/com/dessafio_cdc/DessafioCdcApplication.java
@@ -0,0 +1,13 @@
+package br.com.dessafio_cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessafioCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessafioCdcApplication.class, args);
+	}
+
+}
